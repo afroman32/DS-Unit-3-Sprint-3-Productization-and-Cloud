d9741c28c8fbdd007fc9bd45fd59c65edde1293a
Afroman <makoanoble@gmail.com>
2020-06-11 15:09:53 -0600

    Add form for user to type a username to add to the database

--- module1-web-application-development-with-flask/twitter/__init__.py
+++ module1-web-application-development-with-flask/twitter/__init__.py
@@ -2,12 +2,9 @@ from flask import Flask
 
 from twitter.models import db, migrate
 from twitter.routes.tweet_router import tweets_route
-# from web_app.routes.book_routes import book_routes
+from twitter.routes.stats_routes import stats_routes
 
 DATABASE_URI = "sqlite:///twitter_app.db" # using relative filepath
-#DATABASE_URI = "sqlite:////Users/Username/Desktop/your-repo-name/web_app_99.db" # using absolute filepath on Mac (recommended)
-#DATABASE_URI = "sqlite:///C:\\Users\\Username\\Desktop\\your-repo-name\\web_app_99.db" # using absolute filepath on Windows (recommended) h/t: https://stackoverflow.com/a/19262231/670433
-
 
 def create_app():
     app = Flask(__name__)
@@ -16,8 +13,8 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # app.register_blueprint(home_routes)
     app.register_blueprint(tweets_route)
+    app.register_blueprint(stats_routes)
     return app
 
 if __name__ == "__main__":
--- module1-web-application-development-with-flask/twitter/routes/tweet_router.py
+++ module1-web-application-development-with-flask/twitter/routes/tweet_router.py
@@ -5,6 +5,11 @@ from twitter.services.basilica_services import connection as basilica_api_client
 
 tweets_route = Blueprint("tweets_route", __name__)
 
+@tweets_route.route("/")
+def home():
+    return render_template("prediction_form.html")
+
+
 @tweets_route.route("/tweets.json")
 def list_tweets_json():
     
@@ -18,10 +23,10 @@ def list_tweets_json():
 def list_tweets():
     
     tweet_records = Tweet.query.all()
-    # print(tweet_records)
-    books = parse_records(tweet_records)
+    print(tweet_records)
+    tweets = parse_records(tweet_records)
 
-    return render_template("tweets.html", message="The tweets that be", books=books)
+    return render_template("tweets.html", message="The tweets that be", tweets=tweets)
 
 @tweets_route.route("/tweets/new")
 def new_tweet():
@@ -78,4 +83,50 @@ def get_user(screen_name=None):
         counter+=1
     db.session.commit()
 
-    return render_template("user.html", user=db_user, tweets=statuses) # tweets=db_tweets
+    return render_template("user.html", user=db_user, tweets=statuses)
+
+@tweets_route.route("/users/new")
+def new_user():
+    return render_template("new_user.html")
+
+@tweets_route.route("/create_user", methods=["POST"])
+def create_user(screen_name=None):
+    # print("FORM DATA:", dict(request.form))
+    screen_name = request.form["user"]
+    # print(screen_name)
+
+    twitter_user = twitter_api_client.get_user(screen_name)
+    statuses = twitter_api_client.user_timeline(screen_name, tweet_mode="extended", count=150, exclude_replies=True, include_rts=False)
+    print("STATUSES COUNT:", len(statuses))
+ 
+    # get existing user from the db or initialize a new one:
+    db_user = User.query.get(twitter_user.id) or User(id=twitter_user.id)
+    db_user.screen_name = twitter_user.screen_name
+    db_user.name = twitter_user.name
+    db_user.location = twitter_user.location
+    db_user.followers_count = twitter_user.followers_count
+    db.session.add(db_user)
+    db.session.commit()
+
+    all_tweet_texts = [status.full_text for status in statuses]
+    embeddings = list(basilica_api_client.embed_sentences(all_tweet_texts, model="twitter"))
+    print("NUMBER OF EMBEDDINGS", len(embeddings))
+
+    # TODO: explore using the zip() function maybe...
+    counter = 0
+    for status in statuses:
+        print(status.full_text)
+        print("----")
+
+        # get existing tweet from the db or initialize a new one:
+        db_tweet = Tweet.query.get(status.id) or Tweet(id=status.id)
+        db_tweet.user_id = status.author.id
+        db_tweet.full_text = status.full_text
+        embedding = embeddings[counter]
+        print(len(embedding))
+        db_tweet.embedding = embedding
+        db.session.add(db_tweet)
+        counter+=1
+    db.session.commit()
+
+    return render_template("user.html", user=db_user, tweets=statuses)
